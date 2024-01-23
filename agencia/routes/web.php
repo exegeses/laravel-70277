c7c3584d1f70cc5bd0ebce999a925bc50f1a7649
Marcos <exegeses@gmail.com>
2024-01-22 22:09:33 -0300

    Peticiones y Vistas

--- agencia/routes/web.php
+++ agencia/routes/web.php
@@ -13,6 +13,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});
+*/
+//Route::view('petición', 'vista');
+Route::view('/', 'welcome');
+
+Route::view('/uva', 'inicio');
+
+//closure
+//Route::get('/peticion', acción);
+Route::get('/datos', function()
+{
+    //declarar variables
+    $nombre = 'marcos';
+    $marcas = ['asus', 'inka cola', 'nike', 'adidas', 'puma'];
+    // retornamos una vista y la ENVIAMOS las variables (retornarlas)
+    return view('datos',
+                [
+                    'nombre'=>$nombre,
+                    'marcas'=>$marcas
+                ]
+            );
 });
